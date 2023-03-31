aa703a63902d551bd138c2d51d5cbedf23de1afd
Vitor Roma <39566849+vitorlroma@users.noreply.github.com>
2023-03-30 22:39:28 -0300

    the Play option from the menu became its own function; play() checks whether game_type and players1 and 2 are valid before starting a match; multiple prints for the same type of exception became a single function, error()

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from cards import Deck
 from game import Game
 
 
+def error():
+    print('Invalid input.')
+
+
 def login(players_list):
     nick = input('Login.\nType nickname: ')
     player = players_list.search_player(nick)
@@ -16,16 +20,27 @@ def login(players_list):
     return None
 
 
+def play(players, cards):
+    game_type = 0
+    try:
+        while game_type != 1 or game_type != 2:
+            game_type = int(input('[1]Manual;\n[2]Random.\n'))
+        while player1 is None:
+            player1 = login(players)
+        while player2 is None:
+            player2 = login(players)
+        game = Game(player1, player2, cards, game_type)
+        game.match()
+    except ValueError:
+        error()
+
+
 def menu(players_list, cards):
+    answer = 0
     try:
         answer = int(input('Type:\n[1]Play;\n[2]Register player;\n[3]Check profile;\n[4]Exit.\n'))
         if answer == 1:
-            game_type = int(input('[1]Manual;\n[2]Random.\n'))
-            player1 = login(players_list)
-            player2 = login(players_list)
-            if player1 and player2:
-                game = Game(player1, player2, cards, game_type)
-                game.match()
+            play(players_list, cards)
         elif answer == 2:
             nick = input('Register player.\nType nickname:')
             players_list.add_player(nick)
@@ -35,7 +50,7 @@ def menu(players_list, cards):
         elif answer == 4:
             pass
     except ValueError:
-        print('Undefined answer.\n')
+        error()
 
     return answer
 
