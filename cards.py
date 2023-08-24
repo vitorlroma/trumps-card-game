9769463b283e6e10602ef9e6e44c34510aed7803
Alsn2200 <alsncosta2200@gmai.com>
2023-08-24 10:50:46 -0300

    Class attributes modified to private, var 'file' changed to 'arq'

--- cards.py
+++ cards.py
@@ -4,52 +4,48 @@ from card import Card
 
 
 class Deck:
-
-    def __init__(self, cards):
-        self._deck = cards
+    def __init__(self):
+        self.__deck = []
 
     @property
     def deck(self):
-        return self._deck
+        return self.__deck
 
-    def deck_copy(self):
-        return self._deck.copy()
+    def deck_shuffled(self):
+        return rd.shuffle(self.__deck.copy())
 
     def add_card(self, card):
-        self._deck.append(card)
-
-    def shuffle_cards(self):
-        rd.shuffle(self._deck)
+        self.__deck.append(card)
 
     def give_card(self):
-        return self._deck.pop()
+        return self.__deck.pop()
 
     def get_card(self, index):
-        return self._deck.pop(index)
+        return self.__deck.pop(index)
 
     def rd_card(self):
-        num = rd.randint(0, len(self._deck)-1)
-        return self._deck.pop(num)
+        num = rd.randint(0, len(self.__deck)-1)
+        return self.__deck.pop(num)
 
     def is_empty(self):
-        if not self._deck:
+        if not self.__deck:
             return True
         else:
             return False
 
     def read_cards(self):
-        file = 'cards.json'
+        arq = 'cards.json'
         try:
-            with open(file) as f:
+            with open(arq) as f:
                 data = json.load(f)
 
             for card in data['cards']:
                 new_card = Card(card['Character'], card['Value'], card['Strength'], card['Energy'], card['Jokenpo'])
-                self._deck.append(new_card)
+                self.__deck.append(new_card)
 
         except IOError:
             print('File could not be read.')
 
     def __str__(self):
-        for card in self._deck:
+        for card in self.__deck:
             print(card.__str__())
