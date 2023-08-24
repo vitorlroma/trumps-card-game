930e4c555c47de7a29d17bcff1d5ee73e9b6fdc5
Alsn2200 <alsncosta2200@gmai.com>
2023-08-24 11:02:27 -0300

    Applying OOP, WIP. Having trouble trying to initializa an empty class and giving values later.

--- main.py
+++ main.py
@@ -3,72 +3,72 @@ from cards import Deck
 from game import Game
 
 
-def error():
-    print('Invalid input.')
+class Start:
+    def __init__(self):
+        self.__players_list = Players()
+        self.__cards = Deck()
+        self.__game = Game()
 
+    def __login(self, nick):
+        player = self.__players_list.search_player(nick)
+        if player is None:
+            answer = input('\nWish to register?"Y" or "N"')
+            if answer == "Y" or answer == "y":
+                player = self.__players_list.add_player(nick)
 
-def login(players_list):
-    nick = input('Login.\nType nickname: ')
-    player = players_list.search_player(nick)
-    if player is not None:
-        return player
+        if player is not None and self.__game.player1 is None:
+            self.__game.player1(player)
+        elif player is not None and self.__game.player2 is not None:
+            self.__game.player2(player)
 
-    answer = input('\nWish to register?"Y" or "N"')
-    if answer == "Y" or answer == "y":
-        return players_list.add_player(nick)
+    def __play(self):
+        cond = False
+        while not cond:
+            try:
+                game_type = int(input('[1]Manual;\n[2]Random.\n'))
+                cond = bool(game_type == 1 or game_type == 2)
+            except ValueError:
+                print('Invalid input.')
 
-    return None
+        nick = input('Type your nickname: ')
+        self.__login(nick)
+        nick = input('Type your nickname: ')
+        self.__login(nick)
 
+        self.__game.cards(self.__cards.deck_shuffled())
+        self.__game.match()
 
-def play(players, cards):
-    cond = False
-    while not cond:
+    def read_data(self):
+        self.__players_list.read_players()
+        self.__cards.read_cards()
+
+    def menu(self):
+        answer = 0
         try:
-            game_type = int(input('[1]Manual;\n[2]Random.\n'))
-            cond = bool(game_type == 1 or game_type == 2)
+            answer = int(input('Type:\n[1]Play;\n[2]Register player;\n[3]Check profile;\n[4]Exit.\n'))
+            if answer == 1:
+                self.__play()
+            elif answer == 2:
+                nick = input('Register player.\nType nickname:')
+                self.__players_list.add_player(nick)
+            elif answer == 3:
+                nick = input('\nType nickname:')
+                print(f'\n{self.__players_list.search_player(nick)}\n')
         except ValueError:
-            error()
+            print('Invalid input.')
 
-    player1, player2 = None, None
-    while player1 is None:
-        player1 = login(players)
-    while player2 is None:
-        player2 = login(players)
-    game = Game(player1, player2, cards, game_type)
-    game.match()
-
-
-def menu(players_list, cards):
-    answer = 0
-    try:
-        answer = int(input('Type:\n[1]Play;\n[2]Register player;\n[3]Check profile;\n[4]Exit.\n'))
-        if answer == 1:
-            play(players_list, cards)
-        elif answer == 2:
-            nick = input('Register player.\nType nickname:')
-            players_list.add_player(nick)
-        elif answer == 3:
-            nick = input('\nType nickname:')
-            print(f'\n{players_list.search_player(nick)}\n')
-        elif answer == 4:
-            pass
-    except ValueError:
-        error()
-
-    return answer
+        return answer
 
 
 def main():
     answer = 0
-    players_list = Players()
-    players_list.read_players()
-    cards = Deck([])
-    cards.read_cards()
+    start = Start()
+    start.read_data()
 
     while answer != 4:
-        answer = menu(players_list, cards)
+        answer = start.menu()
 
-    players_list.players_bin()
+    # players_list.players_bin()
 
 
 if __name__ == '__main__':
